4c8c5593b27bfdf76fc2b2822cc87c0e24147c2b
Cristina Grigoruta <cristina.grigoruta@gmail.com>
2012-10-08 02:52:29 +0100

    Fix the display of long arguments and topic titles.

--- src/main/webapp/resources/scripts/jReply.js
+++ src/main/webapp/resources/scripts/jReply.js
@@ -5,13 +5,10 @@ $(document).ready(function() {
 		var parentId = $(this).parent().parent().parent().attr("id");
 		var htmlform = 
 			'<form id="commsubform">'+
-			//'<select name="agreeing'+parentId+'"><option value="true">agree</option><option value="false">disagree</option></select><br />'+
 			'<span style="margin-right: 10px">'+
 				'<input type="radio" name="agreeing'+parentId+'" value="true" checked> Agree </input>'	+
 				'<input type="radio" name="agreeing'+parentId+'" value="false"> Disagree </input></span><br />'	+
-			//'(Bubble thinks this is: <span id="sentiment'+parentId+'">' + comment.sentiment+ '</span>)<br />' +
-			//'<textarea id="thecomment'+parentId+'" class="commtxtfield" onkeypress="checkKey('+parentId+')"></textarea><br /><br /> '+
-			'<textarea id="thecomment'+parentId+'" class="commtxtfield"></textarea><br /><br /> '+
+			'<textarea id="thecomment'+parentId+'" maxlength="140" rows="5" class="commtxtfield"></textarea><br /><br /> '+
 			'<a class="savecommbtn" onclick="addComment(\''+parentId+'\')">Save Comment</a> <input type="checkbox" name="tweetMe'+parentId+'" value="true"/> <img src="/Bubble/resources/social/twitter/twitter_newbird_blue.png" alt="tweetMe"/></form>';
 
 		// toggle between show/hide comment form views
